158f58b0243ec65aae8cbf117b6e050bda502dba
Berrones <berrones.edwin@hotmail.com>
2019-06-26 12:01:02 -0500

    Aplicacion Terminada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LineaComponent } from './components/linea/linea.component';
+import { DonaComponent } from './components/dona/dona.component';
+import { RadarComponent } from './components/radar/radar.component';
+import { BarraComponent } from './components/barra/barra.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'line', component: LineaComponent },
+  { path: 'donut', component: DonaComponent },
+  { path: 'radar', component: RadarComponent },
+  { path: 'bar', component: BarraComponent },
+  { path: '**', redirectTo: 'line', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Charts
+import { ChartsModule } from 'ng2-charts';
+import { LineaComponent } from './components/linea/linea.component';
+import { BarraComponent } from './components/barra/barra.component';
+import { DonaComponent } from './components/dona/dona.component';
+import { RadarComponent } from './components/radar/radar.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LineaComponent,
+    BarraComponent,
+    DonaComponent,
+    RadarComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
